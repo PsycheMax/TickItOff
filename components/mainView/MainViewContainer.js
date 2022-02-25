bdc69985cb9421b99e10024094413a110e464bf2
PsycheMax <psychemax@gmail.com>
2022-02-25 12:19:51 +0000

    -The basic idea has been implemented

--- components/mainView/MainViewContainer.js
+++ components/mainView/MainViewContainer.js
@@ -1,32 +1,35 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { VStack, Button, Center, Flex, Box, Pressable, Text } from 'native-base';
+import { VStack, Button, Center, Flex, Box, Pressable, Text, ScrollView } from 'native-base';
+import EditUserForm from '../users/UserForms/EditUserForm';
 
-import { LoggedUserContext } from '../../utils/UserManager';
+export const RouterLikeContext = React.createContext({
+    changeCurrentViewTo: (targetView, propsForNewView) => { }
+})
 
 const MainViewContainer = (props) => {
 
-    const loggedUser = useContext(LoggedUserContext).userData;
-
-    useEffect(() => {
-        // setUserData(loggedUserData);
-        return () => {
-            // Called when component destroyed
-
-        }
-    }, [])
-
-    function renderProjects() {
-        // return useLoggedUserData(false).projects
-        return "CULO";
-    }
+    const [currentView, setCurrentView] = useState("editUserForm");
 
 
-    return (
-        <Box bgColor={"red.500"} h={"100%"} minW={"100%"} width="100%" minW={"100%"} _web={{ pt: 25 }} pt={"5%"}>
-            <Text>The idea here is to create a context that affects this component: calling the CONTEXT.Functions() will modify this component, showing different things
+    function renderCurrentView() {
+        if (currentView === "editUserForm") {
+            return <EditUserForm />
+        } else {
+            return <Text>The idea here is to create a context that affects this component: calling the CONTEXT.Functions() will modify this component, showing different things
                 Maybe via a switch (context.state.currentView) case (profile): break; or something similar
             </Text>
-        </Box>
+        }
+
+    }
+
+    return (
+        <RouterLikeContext.Provider value={{
+
+        }} >
+            <ScrollView h={"100%"} minW={"100%"} width="100%" minW={"100%"} _web={{ pt: 25 }} pt={"5%"}>
+                {renderCurrentView()}
+            </ScrollView>
+        </RouterLikeContext.Provider>
     )
 }
 
